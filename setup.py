437711fcebf0f62ced3200ea27ac4ce2423273d9
Swaroop C H <swaroop@swaroopch.com>
2020-02-28 10:40:20 -0800

    comment import checks

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ with open('requirements.txt') as f:
     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
 
 
-try:
-    import numpy  # NOQA
-except ImportError:
-    print('numpy is required during installation')
-    sys.exit(1)
+# try:
+#     import numpy  # NOQA
+# except ImportError:
+#     print('numpy is required during installation')
+#     sys.exit(1)
 
-try:
-    import scipy  # NOQA
-except ImportError:
-    print('scipy is required during installation')
-    sys.exit(1)
+# try:
+#     import scipy  # NOQA
+# except ImportError:
+#     print('scipy is required during installation')
+#     sys.exit(1)
 
 setup(
     name='spherecluster',
